7e007a4e62e3a2afbb522869589f158da621a27d
Olzhas Aitkaliyev <o.aitkaliyev@innopolis.ru>
2016-11-21 20:41:24 +0300

    Lol

---
+++ showPlays.php
@@ -0,0 +1,26 @@
+<?php
+	$t = $_REQUEST["t"];
+	
+	$db_theatres = pg_connect("host=localhost port=5432 dbname=Theatres user=postgres password=1234");
+						
+	if(!$db_theatres){
+		echo'No connection :(';
+	}
+	$return = '';
+	
+	$query = "SELECT per.per_name, p.play_date, p.play_time, p.grade, p.price
+				FROM Play p, Theater t, Performance per
+				WHERE p.tid = t.tid 
+				AND per.per_id = p.per_id
+				AND t.name = 'Bolshoi Theatre'";
+
+
+	$query = "SELECT DISTINCT T.name, T.tid FROM theater T WHERE city = '".$t."'";
+	$result = pg_query($db_theatres, $query);
+	while($row = pg_fetch_row($result)){
+		$return = $return.'<option value="'.$row[1].'">' .$row[0].' </option> ';
+	}
+	pg_close($db_theatres);
+	
+	echo $return;
+?>
