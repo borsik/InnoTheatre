4a4e8abd431222d5885b947f70dee7f45b99cf95
Olzhas Aitkaliyev <o.aitkaliyev@innopolis.ru>
2016-11-28 18:22:47 +0300

    Config deleted

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:5432');
-   define('DB_USERNAME', 'Theatres');
-   define('DB_PASSWORD', '1234');
-   define('DB_DATABASE', 'postgres');
-   $db = pg_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
